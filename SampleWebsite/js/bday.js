a98082c854c714b6cb0b2abe29c579628cf91708
vigneshwarbalaji <vignesh.shanmug@anywhere.co>
2020-04-04 10:08:57 +0530

    including new page games.html will be including js features soon

---
+++ SampleWebsite/js/bday.js
@@ -0,0 +1,7 @@
+/*function myFunction() {
+    var x = document.getElementById("month").value;
+
+    if (x.localeCompare("january")) {
+        document.getElementById("gamres").innerHTML = "You are a kind person";
+    }
+}*/
